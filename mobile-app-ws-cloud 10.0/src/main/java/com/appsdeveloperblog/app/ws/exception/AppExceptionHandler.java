45ff034ddd72702c23e59feb9a5bcfccc9fb45fa
sanketjain <sanket.jain@columbia.edu>
2021-02-26 13:06:40 +0530

    Return Custom Error Object Representation

--- mobile-app-ws-cloud 10.0/src/main/java/com/appsdeveloperblog/app/ws/exception/AppExceptionHandler.java
+++ mobile-app-ws-cloud 10.0/src/main/java/com/appsdeveloperblog/app/ws/exception/AppExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.appsdeveloperblog.app.ws.exception;
 
+import java.util.Date;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,12 +9,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.appsdeveloperblog.app.ws.ui.model.response.ErrorMessage;
+
 @ControllerAdvice
 public class AppExceptionHandler {
 
 	@ExceptionHandler(value = UserServiceException.class)
 	public ResponseEntity<Object> handleUserServiceException(UserServiceException ex, WebRequest request) {
-		return new ResponseEntity<>(ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+
+		ErrorMessage errorMessage = new ErrorMessage(new Date(), ex.getMessage());
+
+		return new ResponseEntity<>(errorMessage, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 }
