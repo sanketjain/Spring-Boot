eb143daa2e3adb53a644fbde00cca4dc4e2192f1
sanketjain <sanket.jain@columbia.edu>
2021-03-04 18:54:27 +0530

    Add dependency to seperately deploy on tomcat server by creating war file

--- mobile-app-ws-cloud 10.0/src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsCloudApplication.java
+++ mobile-app-ws-cloud 10.0/src/main/java/com/appsdeveloperblog/app/ws/MobileAppWsCloudApplication.java
@@ -2,18 +2,25 @@ package com.appsdeveloperblog.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.appsdeveloperblog.app.ws.security.AppProperties;
 
 @SpringBootApplication
-public class MobileAppWsCloudApplication {
+public class MobileAppWsCloudApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MobileAppWsCloudApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsCloudApplication.class, args);
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -23,11 +30,11 @@ public class MobileAppWsCloudApplication {
 	public SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
 	}
-	
-	//Used to read token string from a property file
+
+	// Used to read token string from a property file
 	@Bean(name = "AppProperties")
 	public AppProperties getAppProperties() {
 		return new AppProperties();
 	}
-	
+
 }
