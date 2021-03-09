9724ebcddb6ec59db976a386bccca3c69d28c297
sanketjain <sanket.jain@columbia.edu>
2021-03-09 13:13:51 +0530

    Get List of Address Web Service EndPoint

--- mobile-app-ws-cloud 10.0/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ mobile-app-ws-cloud 10.0/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -18,8 +19,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appsdeveloperblog.app.ws.service.UserService;
+import com.appsdeveloperblog.app.ws.shared.dto.AddressDTO;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.app.ws.ui.model.response.AddressesRest;
 import com.appsdeveloperblog.app.ws.ui.model.response.OperationStatusModel;
 import com.appsdeveloperblog.app.ws.ui.model.response.RequestOperationStatus;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
@@ -123,4 +126,19 @@ public class UserController {
 		return returnValue;
 	}
 
+	@GetMapping(path="/{id}/addresses", 
+			produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public List<AddressesRest> getUserAddresses(@PathVariable String id) {
+		List<AddressesRest> returnValue = new ArrayList<>();
+		
+		List<AddressDTO> addressDTO = addressesService.getAddresses(id);
+		
+		if(addressDTO!=null && !addressDTO.isEmpty()) {
+			java.lang.reflect.Type listType = new TypeToken<List<AddressesRest>>() {}.getType();
+			returnValue = new ModelMapper().map(addressDTO, listType);
+		}
+
+		return returnValue;
+	}
+	
 }
