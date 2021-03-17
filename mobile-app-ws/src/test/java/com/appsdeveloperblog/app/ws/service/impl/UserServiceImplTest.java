89243864a3d271a28d12c435211c8aab50ed3a2a
sanketjain <sanket.jain@columbia.edu>
2021-03-17 13:50:51 +0530

    Mocking Objects with Mockito @Mock

--- mobile-app-ws/src/test/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImplTest.java
+++ mobile-app-ws/src/test/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImplTest.java
@@ -1,19 +1,38 @@
 package com.appsdeveloperblog.app.ws.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.io.repositories.UserRepository;
 
 class UserServiceImplTest {
 
+	@InjectMocks
+	UserServiceImpl userService;
+
+	@Mock
+	UserRepository userRepository;
+
 	@BeforeEach
 	void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test
 	void testGetUser() {
-		fail("Not yet implemented");
+		UserEntity userEntity = new UserEntity();
+		userEntity.setId(1L);
+		userEntity.setFirstName("Sanket");
+		userEntity.setUserId("ad235jls35s");
+		userEntity.setEncryptedPassword("adstwekslwetjettt");
+
+		when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
 	}
 
 }
