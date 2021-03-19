bec6ac244b88d5001d83fa8ca2baa7fd400f930e
sanketjain <sanket.jain@columbia.edu>
2021-03-19 11:22:51 +0530

    Testing Rest Controller Method

---
+++ mobile-app-ws/src/test/java/com/appsdeveloperblog/app/ws/ui/controller/UserControllerTest.java
@@ -0,0 +1,22 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGetUserString() {
+		fail("Not yet implemented");
+	}
+
+}
