cb5479e26766d723d194785e78d38a9a13d05730
sanketjain <sanket.jain@columbia.edu>
2021-07-03 12:04:49 +0530

    Update UserRepository.java

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepository.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.app.ws.io.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,13 @@ import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
 	UserEntity findByEmail(String email);
+
 	UserEntity findByUserId(String userId);
+
 	UserEntity findUserByEmailVerificationToken(String token);
+
+	@Query(value = "Select * from Users u where u.EMAIL_VERIFICATION_STATUS='true'", 
+			countQuery = "Select count(*) from Users u where u.EMAIL_VERIFICATION_STATUS='true'", 
+			nativeQuery = true)
+	Page<UserEntity> findAllUsersWithConfirmedEmailAddress(Pageable pageableRequest);
 }
